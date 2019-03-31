8677aa35b76181a507385729b3644e41e88fccc4
Elvis Feagin <47120736+ElvisFeagin@users.noreply.github.com>
2019-03-31 09:45:23 -0700

    31 - Using Services with Dependency Injection

--- src/app/persons/persons.component.ts
+++ src/app/persons/persons.component.ts
@@ -1,9 +1,14 @@
 import { Component, Input } from '@angular/core';
+import { PersonsService } from './persons.service';
 
 @Component({
   selector: 'app-persons',
   templateUrl: './persons.component.html'
 })
 export class PersonsComponent {
-  @Input() personList: string[];
+  personList: string[];
+
+  constructor(prsService: PersonsService) {
+    this.personList = prsService.persons;
+  }
 }
